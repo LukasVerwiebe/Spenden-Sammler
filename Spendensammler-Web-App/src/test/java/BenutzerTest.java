d10f8c857087df5be9cc784b85d416b1d0317b42
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-12-12 09:34:44 +0100

    test

--- Spendensammler-Web-App/src/test/java/BenutzerTest.java
+++ Spendensammler-Web-App/src/test/java/BenutzerTest.java
@@ -74,33 +74,33 @@ public class BenutzerTest {
     
     @Test
     void testCreateQuittung1() {
-        /*userService.createNewQuittung(quittung1);
+//        userService.createNewQuittung(quittung1);
+//        
+//        assertEquals(userService.findAllQuittungen().get(userService.findAllQuittungen().size() - 1).getTextQuittung(), quittung1.getTextQuittung());
+//        
+//        userService.updateQuittung(quittung1.getIdQuittung(), 1.00, "textUpdate");
+//        assertEquals(userService.findAllQuittungen().get(userService.findAllQuittungen().size() - 1).getTextQuittung(), "textUpdate");
+//        
+//        int listGroesse = userService.findAllQuittungen().size();
+//        userService.removeQuittung(quittung1);
+//        assertEquals(listGroesse - 1, userService.findAllQuittungen().size());
         
-        assertEquals(userService.findAllQuittungen().get(userService.findAllQuittungen().size() - 1).getTextQuittung(), quittung1.getTextQuittung());
-        
-        userService.updateQuittung(quittung1.getIdQuittung(), 1.00, "textUpdate");
-        assertEquals(userService.findAllQuittungen().get(userService.findAllQuittungen().size() - 1).getTextQuittung(), "textUpdate");
-        
-        int listGroesse = userService.findAllQuittungen().size();
-        userService.removeQuittung(quittung1);
-        assertEquals(listGroesse - 1, userService.findAllQuittungen().size());
-        */
         
     }
     
     @Test
     void testDiagramm1() {
-        /*int listGroesse1 = userService.diagrammQuittungen(2022).size();
+//        int listGroesse1 = userService.diagrammQuittungen(2022).size();
+//        
+//        userService.createNewQuittung(quittung1);
+//        assertEquals(userService.findAllQuittungen().get(userService.findAllQuittungen().size() - 1).getTextQuittung(), quittung1.getTextQuittung());
+//        assertEquals(userService.diagrammQuittungen(2022).size(), listGroesse1 + 1);
+//                
+//        int listGroesse = userService.findAllQuittungen().size();
+//        userService.removeQuittung(quittung1);
+//        assertEquals(listGroesse - 1, userService.findAllQuittungen().size());
         
-        userService.createNewQuittung(quittung1);
-        assertEquals(userService.findAllQuittungen().get(userService.findAllQuittungen().size() - 1).getTextQuittung(), quittung1.getTextQuittung());
-        assertEquals(userService.diagrammQuittungen(2022).size(), listGroesse1 + 1);
-                
-        int listGroesse = userService.findAllQuittungen().size();
-        userService.removeQuittung(quittung1);
-        assertEquals(listGroesse - 1, userService.findAllQuittungen().size());
         
-        */
     }
 
 }    
